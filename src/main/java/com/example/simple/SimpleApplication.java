e7014a5a4c90112aa5aa4f4206d8ec4ee64604e3
Nghi Nguyen <donnghi8985@gmail.com>
2018-07-07 18:34:55 +0200

    Update Spring Boot Web Application

--- src/main/java/com/example/simple/SimpleApplication.java
+++ src/main/java/com/example/simple/SimpleApplication.java
@@ -2,9 +2,16 @@ package com.example.simple;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SimpleApplication {
+public class SimpleApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SimpleApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleApplication.class, args);
